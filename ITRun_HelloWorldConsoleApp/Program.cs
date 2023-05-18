12900475d3259bf73fb6086a3c6bab36d2cd3812
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-05-18 19:47:01 +0500

    Add project files.

---
+++ ITRun_HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System;
+
+Console.OutputEncoding = Encoding.UTF8;
+decimal money = decimal.Parse(Console.ReadLine());
+decimal firstMoney = money;
+int month = int.Parse(Console.ReadLine());
+for (int i = 1; i <= month; i++)
+{
+    var percentValue = money * 0.07M;
+    money += percentValue;
+    Console.WriteLine($"Моҳи {i} - {money}");
+}
+Console.WriteLine($"Фоидаи шумо {money - firstMoney} аст");
+Console.ReadLine();
---
+++ ITRun_Store/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+string[] products = File.ReadAllLines("store.txt", Encoding.UTF8);
+
+string newlyadded = "Себ";
+int count = 10;
+
+for (int i = 0; i < products.Length; i++)
+{
+    string product = products[i];
+    var splitted = product.Split(' ');
+
+    string productName = splitted[0];
+    int countOfProduct = int.Parse(splitted[1]);
+    if (productName == newlyadded)
+    {
+        countOfProduct += count;
+    }
+    products[i] = $"{productName} {countOfProduct}";
+}
+
+
+string[] added = new string[products.Length + 1];
+for (int i = 0; i < products.Length; i++)
+{
+    added[i] = products[i];
+}
+
+added[products.Length] = "Нок 20";
+
+File.WriteAllLines("store.txt", added, Encoding.UTF8);
+
+Console.ReadLine();
