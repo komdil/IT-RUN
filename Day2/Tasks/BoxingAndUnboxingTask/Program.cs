e5b1af955dae25880b9330860d0aafc0855be199
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-05-23 14:07:27 +0500

    add task for 2 day

---
+++ Day2/Tasks/BoxingAndUnboxingTask/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Code1()
+{
+    int intfive = 5;
+    object objFive = intfive;
+
+    int intFive2 = 5;
+    object objFive2 = intFive2;
+
+    bool isIntEqual = intfive == intFive2;
+    Console.WriteLine(isIntEqual);
+
+    bool isObjEqual = objFive == objFive2;
+    Console.WriteLine(isObjEqual);
+}
+
+void Code2()
+{
+    string strfive = "5";
+    object objFive = strfive;
+
+    string strFive2 = "5";
+    object objFive2 = strFive2;
+
+    bool isIntEqual = strfive == strFive2;
+    Console.WriteLine(isIntEqual);
+
+    bool isObjEqual = objFive == objFive2;
+    Console.WriteLine(isObjEqual);
+}
+
+void Code3()
+{
+    int x = 5;
+    object o = x;
+    x = 10;
+    var y = (int)o;
+    Console.WriteLine(y);
+}
+
+Console.ReadLine();
---
+++ Day2/Tasks/MemoryTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        int maxSize = int.Parse(Console.ReadLine());
+        string result = Generate(number, maxSize);
+        Console.Write(result);
+        Console.ReadLine();
+    }
+
+    static string Generate(int number, int maxSize)
+    {
+        string result = "";
+
+        for (int i = 1; i <= number; i++)
+        {
+            for (int j = 1; j <= maxSize; j++)
+            {
+                result += $"{i}x{j}={i * j}";
+                result += "\n";
+            }
+        }
+        return result;
+    }
+}
