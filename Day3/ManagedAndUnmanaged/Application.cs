a9e07a576647f2d72bf2829313aab8540fbc0d68
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-05-25 18:54:52 +0500

    day3

---
+++ Day3/ManagedAndUnmanaged/Application.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedAndUnmanaged
+{
+    public class Application : IDisposable
+    {
+        StreamWriter StreamWriter;
+        public Application()
+        {
+            StreamWriter = new StreamWriter("store.txt", true);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        bool disposed;
+        protected void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                //Clean up managed code
+                StreamWriter.Dispose();
+            }
+            //clean up unmanaged code
+            disposed = true;
+        }
+
+        ~Application()
+        {
+            Dispose(false);
+        }
+
+        public void WriteProduct(string name, int count)
+        {
+            StreamWriter.WriteLine($"{name} {count}");
+            StreamWriter.Flush();
+        }
+    }
+}
