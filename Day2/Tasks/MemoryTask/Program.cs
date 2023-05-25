96bb9b337d5c1872ea41013dd0f2c3b4e1d57bae
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-05-25 15:00:36 +0500

    Added branchmark

--- Day2/Tasks/MemoryTask/Program.cs
+++ Day2/Tasks/MemoryTask/Program.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
 
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text;
+
+[MemoryDiagnoser]
+[RankColumn]
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        int number = int.Parse(Console.ReadLine());
-        int maxSize = int.Parse(Console.ReadLine());
-        string result = Generate(number, maxSize);
-        Console.Write(result);
-        Console.ReadLine();
+        BenchmarkRunner.Run<BenchmarkDiagnose>();
     }
+}
 
-    static string Generate(int number, int maxSize)
+
+public class BenchmarkDiagnose
+{
+    private int number = 10;
+    private int maxSize = 10;
+    public static string Generate(int number, int maxSize)
     {
         string result = "";
 
@@ -24,5 +33,29 @@ class Program
             }
         }
         return result;
+    }
+    [Benchmark]
+    public string TestBenchmark()
+    {
+        return Generate(number, maxSize);
+    }
+
+    [Benchmark]
+    public string TestBenchmarkWithStringBuilder()
+    {
+        return GenerateWithStringBuilder(number, maxSize);
+    }
+    public static string GenerateWithStringBuilder(int number, int maxSize)
+    {
+        var result = new StringBuilder();
+
+        for (int i = 1; i <= number; i++)
+        {
+            for (int j = 1; j <= maxSize; j++)
+            {
+                result.Append($"{i}x{j}={i * j}\n");
+            }
+        }
+        return result.ToString();
     }
 }
