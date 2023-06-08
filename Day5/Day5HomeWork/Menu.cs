96cff295082171482f6b75e37034796c283c37bc
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-06-08 19:43:02 +0500

    day5 homework

---
+++ Day5/Day5HomeWork/Menu.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Day5HomeWork
+{
+    public class Menu : IMenu
+    {
+        public Menu()
+        {
+            Childs = new Menu[0];
+        }
+
+        public string MenuName { get; set; }
+        public Menu[] Childs { get; set; }
+        IMenu[] IMenu.Childs { get => Childs; set => Childs = (Menu[])value; }
+        public string Generate()
+        {
+            return $"<li>{MenuName}</li>";
+        }
+    }
+
+    public class ComplexMenu : IMenu
+    {
+        public ComplexMenu()
+        {
+            Childs = new ComplexMenu[0];
+        }
+
+        public string MenuName { get; set; }
+        public string Href { get; set; }
+        public ComplexMenu[] Childs { get; set; }
+        IMenu[] IMenu.Childs { get => Childs; set => Childs = (ComplexMenu[])value; }
+        public string Generate()
+        {
+            return $"<li><a href='{Href}'>{MenuName}</a></li>";
+        }
+    }
+
+    public interface IMenu
+    {
+        IMenu[] Childs { get; set; }
+
+        string Generate();
+    }
+}
