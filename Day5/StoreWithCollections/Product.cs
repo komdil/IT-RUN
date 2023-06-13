8cc340a9dddd7727d3df5857eaef0774d3c54b3a
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-06-13 13:39:32 +0500

    Day 5 homework

---
+++ Day5/StoreWithCollections/Product.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+public class Product
+{
+    public List<string> prods { get; set; }
+    public Product()
+	{
+        prods = File.ReadAllLines("store.txt", Encoding.UTF8).ToList();
+    }
+
+    public Product(string name, int count)
+    {
+        SaveToFile(name, count);
+    }
+
+    public void SaveToFile(string name, int count)
+    {
+        prods.Add(name + " " + count);  
+        File.WriteAllLines("store.txt", prods);
+    }
+
+    public static string[] GetPorducts(string filePath)
+    {
+        return File.ReadAllLines(filePath, Encoding.UTF8);
+    }
+
+    public static bool ProductExists(string name, string filePath)
+    {
+        var prods = File.ReadAllLines(filePath, Encoding.UTF8);
+        foreach (var item in prods)
+        {
+            if (item.StartsWith(name))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void ChangeNumber(string name, int n)
+    {
+        for (int i = 0; i < prods.Count; i++)
+        {
+            if (prods[i].StartsWith(name))
+            {
+                var temp = int.Parse(prods[i].Split()[1]);
+                if (n + temp >= 0)
+                {
+                    prods[i] = name + " " + (temp + n);
+                    File.WriteAllLines("store.txt", prods);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Not enough products");
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Day5/StoreWithCollections/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    Product pr = new Product();
+    Console.WriteLine("a - list of products\nb - add new product\nc - sell product\n");
+    string userInput = Console.ReadLine();
+
+    if (userInput == "a")
+    {
+        foreach (var item in Product.GetPorducts("store.txt"))
+        {
+            Console.WriteLine(item);
+        }
+    }
+    else if (userInput == "b")
+    {
+        Console.Write("Input new product name: ");
+        string newProductName = Console.ReadLine();
+        Console.Write("Input new product quantity: ");
+        int newProductQuantity = int.Parse(Console.ReadLine());
+
+        if (Product.ProductExists(newProductName, "store.txt"))
+        {
+            pr.ChangeNumber(newProductName, newProductQuantity);
+        }
+        else
+        {
+            pr.SaveToFile(newProductName, newProductQuantity);
+        }
+    }
+    else if (userInput == "c")
+    {
+        Console.Write("Input the product name you want to sell: ");
+        string productName = Console.ReadLine();
+        Console.Write("Input the quantity: ");
+        int productQuantity = int.Parse(Console.ReadLine());
+
+        pr.ChangeNumber(productName, -1 * productQuantity);
+    }
+}
