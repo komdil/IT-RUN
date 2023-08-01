0f869f8166621b2e6c54b18ce35b96b3ab686364
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-08-01 19:01:37 +0500

    TDD

---
+++ TDD/Calculator.UnitTests/CalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using MyCalculatorApp;
+
+namespace Calculator.UnitTests
+{
+    public class CalculatorTests
+    {
+        [TestCase(10, 10, 20)]
+        [TestCase(-10, 10, 0)]
+        [TestCase(-10, -10, -20)]
+        public void Calculator_Plus_Test(int number1, int number2, int expectedResult)
+        {
+            MyCalculator calculator = new MyCalculator();
+            int result = calculator.Plus(number1, number2);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase(20D, 10D, 2)]
+        [TestCase(-10D, 10D, -1)]
+        [TestCase(-10D, -10D, 1D)]
+        public void Calculator_Divide_Test(double number1, double number2, double expectedResult)
+        {
+            MyCalculator calculator = new MyCalculator();
+            double result = calculator.Divide(number1, number2);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void Calculator_DivideOnZero_ShouldThrowException_Test()
+        {
+            MyCalculator calculator = new MyCalculator();
+            Assert.Throws<CannotDivideToZeroException>(() =>
+            {
+                calculator.Divide(100, 0);
+            });
+        }
+    }
+}
---
+++ TDD/MyCalculatorApp/MyCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace MyCalculatorApp
+{
+    public class MyCalculator
+    {
+        public double Divide(double number1, double number2)
+        {
+            if (number2 == 0)
+            {
+                throw new CannotDivideToZeroException();
+            }
+
+            return number1 / number2;
+        }
+
+        public int Plus(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
