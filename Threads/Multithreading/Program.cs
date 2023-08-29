46a756158fe386f2d70f473cb9ecd411677f454f
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-08-29 19:50:39 +0500

    BlockingCollection

--- Threads/Multithreading/Program.cs
+++ Threads/Multithreading/Program.cs
@@ -1,18 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Collections.Concurrent;
 using System.Diagnostics.Metrics;
-//TPL
-Console.WriteLine("Hello, World!");
-ConcurrentDictionary<string, string> metrics = new ConcurrentDictionary<string, string>();
+////TPL
+//Console.WriteLine("Hello, World!");
+//ConcurrentDictionary<string, string> metrics = new ConcurrentDictionary<string, string>();
 
-Task[] tasks = new Task[1000];
-for (int i = 0; i < 1000; i++)
+//var threadParam = new ParameterizedThreadStart(RunInNewThread);
+
+//Thread thread = new Thread(threadParam);
+//thread.IsBackground = true;
+//thread.UnsafeStart();
+
+//void RunInNewThread(object? obj)
+//{
+//    Thread.Sleep(5000);
+//    File.WriteAllText("1.txt", "Thread finished");
+//}
+
+
+BlockingCollection<int> values = new BlockingCollection<int>();
+StartCreatePayloads();
+void StartCreatePayloads()
 {
-    tasks[i] = Task.Run(() =>
+    Task.Run(() =>
     {
-        metrics.TryAdd(Guid.NewGuid().ToString(), "Test");
+        for (int i = 0; i < 10; i++)
+        {
+            Task.Delay(1000).Wait();
+            Console.WriteLine($"Item {i} added");
+            values.Add(i);
+        }
+        values.CompleteAdding();
     });
 }
-Task.WaitAll(tasks);
-Console.WriteLine($"Number is {metrics.Count}");
+
+foreach (var item in values.GetConsumingEnumerable())
+{
+    //Send request to web site async
+    Console.WriteLine("Sending request to " + item);
+}
+
 Console.ReadLine();
