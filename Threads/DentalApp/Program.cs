fe405c6df4a8a541b488f45da6aaf77be865dc3b
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-09-04 19:57:40 +0500

    add app

---
+++ Threads/DentalApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Timers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        _ = CheckHardware();
+        _ = CheckDriver();
+        Console.ReadLine();
+    }
+
+    static bool IsConnected()
+    {
+        return false;
+    }
+
+    static async Task CheckHardware()
+    {
+        while (!IsConnected())
+        {
+            await Task.Delay(10);
+        }
+        EnableButton();
+    }
+
+    static async Task CheckDriver()
+    {
+        while (!IsConnected())
+        {
+            await Task.Delay(10);
+        }
+        EnableButton();
+    }
+
+    static void EnableButton()
+    {
+
+    }
+}
