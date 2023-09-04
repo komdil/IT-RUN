def6a18821a2bd46668b68f8b7a5be83f30c2e97
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2023-09-04 16:51:18 +0500

    ad

--- DownloaderApp/DownloaderApp/Form1.Designer.cs
+++ DownloaderApp/DownloaderApp/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -59,7 +63,7 @@
             this.textBox1.Location = new System.Drawing.Point(48, 86);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(909, 46);
+            this.textBox1.Size = new System.Drawing.Size(396, 46);
             this.textBox1.TabIndex = 2;
             // 
             // listBox1
@@ -71,11 +75,51 @@
             this.listBox1.Size = new System.Drawing.Size(386, 279);
             this.listBox1.TabIndex = 3;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(561, 86);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(396, 46);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Download count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(561, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Simultanounsly";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(452, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(418, 124);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1007, 623);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -94,5 +138,9 @@
         private Button button2;
         private TextBox textBox1;
         private ListBox listBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Button button3;
     }
 }
--- DownloaderApp/DownloaderApp/Form1.cs
+++ DownloaderApp/DownloaderApp/Form1.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Windows.Forms.VisualStyles;
 
 namespace DownloaderApp
@@ -14,24 +15,31 @@ namespace DownloaderApp
             MessageBox.Show("You need to activate app");
         }
 
-
+        Semaphore semaphore;
         private void button2_Click(object sender, EventArgs e)
         {
-            var url = textBox1.Text;
-            DownloadFromUrl(url);
+            int sim = int.Parse(textBox2.Text);
+            semaphore = new Semaphore(sim, sim);
+            var downloadCount = int.Parse(textBox1.Text);
+            for (int i = 0; i < downloadCount; i++)
+            {
+                DownloadFromUrl(i);
+            }
         }
 
-        private void DownloadFromUrl(string url)
+        private void DownloadFromUrl(int fileNumber)
         {
             //Download with http client
             Task.Run(() =>
             {
-                Task.Delay(500).Wait();
+                semaphore.WaitOne();
+                Task.Delay(1000).Wait();
+                semaphore.Release();
             }).ContinueWith(s =>
             {
                 this.Invoke(() =>
                 {
-                    listBox1.Items.Add("File downloaded");
+                    listBox1.Items.Add($"File {fileNumber} downloaded");
                 });
             });
         }
@@ -39,6 +47,11 @@ namespace DownloaderApp
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
         }
     }
 }
